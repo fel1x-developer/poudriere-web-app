db4387422d36cd558a43de81a403c06c5e97c92a
Minsoo Choo <minsoochoo0122@proton.me>
2025-02-03 12:17:41 -0500

    Update packages

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
-/** @type { import('@storybook/svelte').Preview } */
-const preview = {
+import type { Preview } from '@storybook/svelte';
+
+const preview: Preview = {
 	parameters: {
 		controls: {
 			matchers: {
