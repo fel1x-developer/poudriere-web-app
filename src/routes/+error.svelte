ef0989765f72f0569481d140fa6ba54dba9a175b
Minsoo Choo <minsoochoo0122@proton.me>
2025-03-08 13:50:41 -0500

    Simplify error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,18 +5,20 @@
 </script>
 
 <main>
-    <div class="flex flex-col gay-y-2 lg:gap-y-3">
-        <div class="flex flex-row items-center justify-center w-full text-5xl lg:text-9xl text-bold gap-x-6 lg:gap-x-15 align-middle">
-            <Fa icon={faFreebsd} />
-            ?
-        </div>
-        <div class="flex flex-col gap-y-1 lg:gap-y-2 align-middle">
-            <h1 class="text-text dark:text-text-dark text-center text-3xl lg:text-6xl font-bold">
-                Oops! Error Occurred!
-            </h1>
-            <h1 class="text-text dark:text-text-dark text-center text-xl lg:text-3xl font-bold">
-                {page.status}: {page.error?.message}
-            </h1>
-        </div>
-    </div>
+	<div class="gay-y-2 flex flex-col gap-y-2 lg:gap-y-3">
+		<div
+			class="text-bold flex w-full flex-row items-center justify-center gap-x-6 align-middle text-5xl lg:gap-x-15 lg:text-9xl"
+		>
+			<Fa icon={faFreebsd} />
+		</div>
+		<div class="flex flex-row align-middle gap-x-3 lg:gap-x-6">
+			<div class="inline-grid *:[grid-area:2/1]">
+				<div class="status status-error animate-ping lg:status-xl"></div>
+				<div class="status status-error lg:status-xl"></div>
+			</div>
+			<h1 class="text-text dark:text-text-dark text-center text-3xl font-bold lg:text-6xl">
+				{page.status}: {page.error?.message}
+			</h1>
+		</div>
+	</div>
 </main>
