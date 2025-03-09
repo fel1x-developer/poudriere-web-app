cdbd21a8b495ce4e3989238d895a9819ec969c83
Minsoo Choo <minsoochoo0122@proton.me>
2025-03-08 19:36:33 -0500

    Simplify error mechanism

--- src/routes/build/[slug]/+page.server.ts
+++ src/routes/build/[slug]/+page.server.ts
@@ -1,10 +1,14 @@
+import {error} from "@sveltejs/kit";
+
 export const prerender = 'auto';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
-	const build: PoudriereBuild = await fetch(`/140-rel-default/${params.slug}/.data.json`).then(
-		(res) => res.json()
-	);
+	const build: PoudriereBuild = await fetch(`/140-rel-default/${params.slug}/.data.json`)
+		.then((res) => res.json())
+		.catch((err) => error(404, {
+			message: err.message,
+		}));
 	return {
 		build
 	};
